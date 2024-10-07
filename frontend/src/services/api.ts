c2f124039eb0d54109259fe42d0f3147ce8d63f9
callebtc <93376500+callebtc@users.noreply.github.com>
2024-10-07 03:37:42 +0200

    update

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -3,8 +3,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: process.env.VITE_API_BASE_URL || 'https://api.audit.8333.space',
-  baseURL: 'https://api.audit.8333.space',
+  baseURL: process.env.VITE_API_BASE_URL || 'https://api.audit.8333.space',
   headers: {
     'Content-Type': 'application/json',
   },
