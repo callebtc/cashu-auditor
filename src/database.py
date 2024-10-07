38d6d67835debfc4b46cc5aba058212398bab9bb
callebtc <93376500+callebtc@users.noreply.github.com>
2024-10-07 18:41:24 +0200

    db no log

--- src/database.py
+++ src/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "sqlite+aiosqlite:///./mints.db"
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(DATABASE_URL)
 
 AsyncSessionLocal = sessionmaker(
     bind=engine, class_=AsyncSession, expire_on_commit=False
