7313e2f128884d2ca0368abecaaf75a09b9696d9
callebtc <93376500+callebtc@users.noreply.github.com>
2025-03-30 14:00:30 +0200

    frontend build

--- frontend/src/components/MintSwapStats.vue
+++ frontend/src/components/MintSwapStats.vue
@@ -334,6 +334,10 @@ export default defineComponent({
   color: white;
   min-height: 100px;
   border-radius: 8px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+  padding-left: 15px;
+  padding-right: 15px;
 }
 
 .rounded-borders {
